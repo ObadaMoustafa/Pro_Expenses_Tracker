47e13f20e48f497bee9627802cc1d339675f3da9
ObadaElSharbatly <obadamoustafa@gmail.com>
2022-02-07 18:57:13 +0100

    try deploying

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,29 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 dotenv.config();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
+if (port === null) {
+  throw new Error("port is null it seems you forgot to create env file");
+}
 
-app.listen(port, () => console.log(`server is running in port ${port}`));
+function startServer() {
+  try {
+    app.listen(port, () => console.log(`server is running in port ${port}`));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(new URL("../../client/dist", import.meta.url).pathname)
+  );
+  // Redirect * requests to give the client data
+  app.get("*", (req, res) =>
+    res.sendFile(
+      new URL("../../client/dist/index.html", import.meta.url).pathname
+    )
+  );
+}
+
+startServer();
