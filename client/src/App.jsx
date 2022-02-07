9bdd952ab914ad154bb0bf0b330c7f6479fd77d9
ObadaElSharbatly <obadamoustafa@gmail.com>
2022-02-07 18:48:30 +0100

    deleted unwanted files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,13 @@ import "./style/App.css";
 function App() {
   const serverSide = process.env.REACT_APP_SERVER_SIDE;
   console.log(serverSide);
+
+  function connectToServer() {
+    fetch(`${serverSide}/api/test`);
+  }
   return (
     <>
-      <form action={`${serverSide}/api/test`} method="GET">
+      <form onSubmit={connectToServer}>
         <button type="submit">connect with server</button>
       </form>
     </>
